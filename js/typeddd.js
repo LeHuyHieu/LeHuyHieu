5d71fd9a2e7957e6f009933d4c390f9fa855f750
LeHuyHieu <lehuyhieupro06182@gmail.com>
2023-06-17 23:38:03 +0700

    first commit

---
+++ js/typeddd.js
@@ -0,0 +1,14 @@
+var typed = new Typed('.typed span', {
+    strings: ["Web designer", "Graphic designer", "Photographer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+});
+var typed = new Typed('.typed2 span', {
+    strings: ["Web designer", "Graphic designer", "Photographer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+});
